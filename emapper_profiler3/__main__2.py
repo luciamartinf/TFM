98a1a19a6ee623932664e70da2901f6275ffd93e
Lucía Martín Fernández <luciamartinfernandez99@gmail.com>
2023-06-04 18:41:52 +0200

    Add files via upload

---
+++ emapper_profiler3/__main__2.py
@@ -0,0 +1,203 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from eggnog_classes2 import Eggnog_sample
+from novelfam_classes2 import NovelFam_sample
+from ko_functions2 import write_tsv, write_json, read_coverm_as_nested_dict, extract_orf_lengths, write_contig_tsv
+#from novelfam_fun import check_novelfam, nf_abundance, check_all_novelfam
+from arg_parse import check_arg
+import json
+import os
+import sys
+import re
+
+#######################
+### PARSE ARGUMENTS ###
+#######################
+
+# Example: python main.py -i data_resume/ -s sample_file.txt -k /Users/lucia/Desktop/TFM/scripts/parse_KEGGpathway_db/KEGG_KOs_dict.txt
+
+arguments = check_arg(sys.argv[1:])
+
+## Get Kegg pathways dictionary
+
+if arguments.kegg_dict:
+    KEGG_dict_file = arguments.kegg_dict
+else:
+    # generar el diccionario a partir del archivo KOs.kegs_cleaned ? No tiene sentido porque tengo que proporcionar tb ese archivo
+    KEGG_dict_file = "/Users/lucia/Desktop/TFM/scripts/parse_KEGGpathway_db/KEGG_pathway_dict.txt"
+
+kos_dict_file = "/Users/lucia/Desktop/TFM/scripts/parse_KEGGpathway_db/KEGG_kos_dict.txt"
+
+# Load the dictionary from the json file
+with open(KEGG_dict_file, "r") as file:
+    KEGG_dict = json.load(file)
+
+with open(kos_dict_file, "r") as file:
+    kos_dict = json.load(file)
+
+## Get input directory and its files
+    
+inputdir = arguments.inputdir
+if not os.path.isdir(inputdir):
+    print('Error: Input directory does not exist')
+
+files = os.listdir(inputdir)
+
+if arguments.nf_dir:
+    novelfam_dir = arguments.nf_dir
+else:
+    novelfam_dir = os.path.join(inputdir+'/novel_families/')
+
+## Get output directory 
+
+outputdir = arguments.outputdir
+
+## Option arguments
+
+remove_euk = arguments.filter_euk
+novel_fam = arguments.novel_fam
+
+
+## Get all sample names
+
+if arguments.sample_file :
+    with open(arguments.sample_file) as f:
+        sample_list = f.read().splitlines()
+else:
+    sample_list = []
+    for file_name in files:
+        basename = os.path.basename(file_name)
+        if '.emapper.annotations' in basename:
+            samplename = re.sub(r'.emapper.annotations', '', basename)
+            sample_list.append(samplename)
+
+## select units
+
+units_option = arguments.unit
+
+## coverm suffix
+
+coverm_suffix = arguments.coverm_suffix
+
+# Create output directory if it does not exist
+if not os.path.isdir(outputdir):
+    os.mkdir(outputdir)
+
+ko_abun_file = os.path.join(outputdir, 'ko_abundance.tsv')
+og_abun_file = os.path.join(outputdir, 'og_abundance.tsv')
+og_contig_file = os.path.join(outputdir, 'og_contig_abun.tsv')
+ko_contig_file = os.path.join(outputdir, 'ko_contig_abun.tsv')
+path_cov_file = os.path.join(outputdir, 'pathway_coverage.tsv')
+
+if not os.path.isdir(outputdir+'/ko_contig_abun'):
+    os.mkdir(outputdir+'/ko_contig_abun')
+if not os.path.isdir(outputdir+'/og_contig_abun'):
+    os.mkdir(outputdir+'/og_contig_abun')
+
+
+#########################
+### PROGRAM EXECUTION ###
+#########################
+
+ko_abundance_all = {}
+#ko_abundance_all['UNMAPPED'] = {}
+path_coverage = {}
+og_abundance_all = {}
+nf_abundance_all = {}
+og_contig_abun = {}
+ko_contig_abun = {}
+nf_contig_abun = {}
+
+Eggnog_sample.init_unit(units_option)
+Eggnog_sample.init_sample_list(sample_list)
+
+if novel_fam :
+    nf_abun_file = os.path.join(outputdir, 'nf_abundance.tsv')
+    NovelFam_sample.init_unit(units_option)
+    NovelFam_sample.init_sample_list(sample_list)
+    if not os.path.isdir(outputdir+'/nf_contig_abun'):
+        os.mkdir(outputdir+'/nf_contig_abun')
+
+for sample in sample_list:
+
+    print('Starting processing sample {}'.format(sample))
+
+    # Define eggnog and coverm filenames
+    eggnog_file = os.path.join(inputdir, sample + '.emapper.annotations')
+    coverm_file = os.path.join(inputdir, sample + coverm_suffix)
+    genepred_file = os.path.join(inputdir, sample + '.emapper.genepred.fasta')
+    
+    # Load eggnog and coverm samples    
+    coverm_dict = read_coverm_as_nested_dict(coverm_file, Eggnog_sample.calc_unit) 
+    orf_dict, total = extract_orf_lengths(genepred_file, coverm_dict, Eggnog_sample.calc_unit)
+
+    eggnog_sample = Eggnog_sample(eggnog_file, total, sample, remove_euk)
+    og_abundance_all, og_contig_abun, ko_contig_abun = eggnog_sample.load_sample(orf_dict, coverm_dict, og_abundance_all, og_contig_abun, ko_contig_abun, kos_dict)
+    
+    # write sample files
+    ko_contig_dict = eggnog_sample.contig_ko
+    og_contig_dict = eggnog_sample.contig_og
+    ko_contig_file = os.path.join(outputdir+'/ko_contig_abun', sample+'_ko_contig_abun.tsv')
+    og_contig_file = os.path.join(outputdir+'/og_contig_abun', sample+'_og_contig_abun.tsv')
+    header1='KEGG_ko\tDescription\tContig\tTPM'
+    write_contig_tsv(ko_contig_dict, ko_contig_file, header1, des = True, n=1)
+    header2 = 'OG\tKingdom\tDescription\tContig\tTPM'
+    write_contig_tsv(og_contig_dict, og_contig_file, header2, des = True, king=True, n=2)
+
+    # Add sample abundance and pathways coverage to complete dictionary
+    og_abundance_all = eggnog_sample.calculate_og_abundance(og_abundance_all)
+    ko_abundance_all = eggnog_sample.calculate_ko_abundance(ko_abundance_all, kos_dict)
+    path_coverage = eggnog_sample.calculate_KEGG_pathway_completeness(path_coverage, KEGG_dict)
+    
+    # Repeat process for novel families
+    if novel_fam :
+        novelfam_file = os.path.join(novelfam_dir, sample + '.emapper.annotations')
+        novelfam_sample = NovelFam_sample(novelfam_file, total, sample)
+        nf_contig_abun = novelfam_sample.load_sample(orf_dict, coverm_dict, nf_contig_abun)
+        nf_abundance_all = novelfam_sample.calculate_nf_abundance(nf_abundance_all)
+       
+        # write sample files
+        nf_contig_file = os.path.join(outputdir+'/nf_contig_abun', sample+'_nf_contig_abun.tsv')
+        nf_contig_dict = novelfam_sample.contig_nf
+        header3='Novel_Fam\tContig\tTPM'
+        write_contig_tsv(nf_contig_dict, nf_contig_file, header3, n=0)
+
+    #     #repeated_queries = check_all_novelfam(eggnog_sample, novelfam_sample, sample)
+
+    print('Finished processing sample {}'.format(sample))
+
+
+##########################
+### WRITE OUTPUT FILES ###
+##########################
+
+print('Writing files. Almost done...')
+
+# ko_abundance_all = dict(sorted(ko_abundance_all.items()))
+# path_coverage = dict(sorted(path_coverage.items()))
+# og_abundance_all = dict(sorted(og_abundance_all.items()))
+# nf_abundance_all = dict(sorted(nf_abundance_all.items()))
+
+header1='KEGG_ko\tDescription\t'+ '\t'.join(sample_list)
+write_tsv(ko_abundance_all, ko_abun_file, header1, sample_list, des = True)
+write_json(os.path.join(outputdir, 'ko_abundance.json'), ko_abundance_all)
+
+header2='KEGG_pathway\tDescription\t'+ '\t'.join(sample_list)
+write_tsv(path_coverage, path_cov_file, header2, sample_list, des = True)
+
+header3='OG\tKingdom\tDescription\t'+ '\t'.join(sample_list)
+write_tsv(og_abundance_all, og_abun_file, header3, sample_list, des = True, king = True)
+
+
+header4='OG\tKingdom\tDescription\t'+ '\t'.join(sample_list)
+write_tsv(og_contig_abun, og_contig_file, header4, sample_list, des = True, king = True)
+
+header6='OG\tDescription\t'+ '\t'.join(sample_list)
+write_tsv(ko_contig_abun, ko_contig_file, header6, sample_list, des = True)
+
+if novel_fam :
+    header5='Novel_Fam\t'+ '\t'.join(sample_list) #+ '\tCOG_match'
+    write_tsv(nf_abundance_all, nf_abun_file, header5, sample_list) #, cog=True)
+
+
---
+++ emapper_profiler3/novelfam_classes2.py
@@ -0,0 +1,181 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from dataclasses import dataclass, field
+from ko_functions2 import check_unmapped, check_key
+import numpy as np
+import re
+import os
+
+# @dataclass
+# class NovelFam_orf(object):
+
+#     """
+#     Dataclass to store novel families' attributes corresponding to one orf
+#     """
+
+#     query: str
+#     #target: str
+#     #evalue: str # = field(repr=False) #1.25e-07?? 
+#     #score: float # = field(repr=False)
+#     novel_fam: str
+#     contig: str #= field(init=False) 
+
+#     def __str__(self):
+
+#         """
+#         Instance method to define the print format of the instance
+#         """
+
+#         row_list = [self.query, self.novel_fam, self.contig]
+#         s = '\t'.join([str(item) for item in row_list])
+        
+#         return s
+
+
+
+class NovelFam_sample(object):
+
+    """
+    Class to store all rows from an eggnog-mapper file. 
+    Each row is a eggnog_orf instance
+    """
+
+    option_unit = None # the option unit as given in the arguments
+    calc_unit = None # the unit we use for calculations
+    sample_list = None
+
+    def __init__(self, filename: str, total_sample:float, samplename = None) -> None:
+        self.filename = filename
+        if samplename == None :
+            self.samplename = self.define_samplename() # function to extract samplename from filename
+        else: 
+            self.samplename = samplename
+        # self.query_list = []
+        # self.query_dict = {}
+        self.total = total_sample
+        self.mapped = 0
+        self.nf_abundance = {}
+        self.contig_nf = {}
+
+    @classmethod
+    def init_unit(cls, given_unit):
+
+        # dictionary to transform argument options into coverm units names 
+        unit_dict ={'rpkm': 'RPKM', 'tpm': 'TPM', 'tm':'Trimmed Mean'} 
+        units = unit_dict[given_unit] 
+
+        NovelFam_sample.option_unit = units
+        
+        # if the parsed unit is TPM, we will use RPKM for calculations and then transform it to TPM
+        if given_unit == 'tpm': 
+            NovelFam_sample.calc_unit = 'RPKM'
+        else:
+            NovelFam_sample.calc_unit = units
+    
+    @classmethod
+    def init_sample_list(cls, sample_list):
+        NovelFam_sample.sample_list = sample_list
+
+    def define_samplename(self):
+
+        """
+        Instance method to extract samplename from the filename
+        """
+
+        basename = os.path.basename(self.filename)
+        self.samplename = re.sub(r'.emapper.*', '', basename) # filename needs to be of format: sample.emmapper.annotations
+        
+        return self.samplename
+    
+    def load_sample(self, orf_dict, coverm_dict, nf_dict2):
+
+        """
+        Instance method to load all rows from a file as eggnog_orf instances and store them as a 
+        eggnog_sample instance
+        """
+        
+        with open(self.filename,"r") as file:
+            for line in file:
+                if not line.startswith("##"): # skip headers
+
+                    if line.startswith("#"):
+                        self.header = line.strip() #.split('\t') # column names as string, eliminar #??
+                    else:  # read lines
+                        items = line.strip().split('\t')
+                        novel_fam = items[4]
+                        query = items[0]
+                        abundance = orf_dict[query]['abundance']
+                        contig = re.sub(r'_[0-9]*$', '', query)
+                        tpm = coverm_dict[contig]['TPM']
+
+                        self.add_nf_abundance(novel_fam, abundance)
+                        self.mapped += abundance
+
+                        nf_dict2 = self.calc_contig_nf_abundance(novel_fam, contig, tpm, nf_dict2)
+
+                        # self.query_list.append(query)
+                        # self.query_dict[novel_fam] = query
+        
+        return nf_dict2
+
+    def add_nf_abundance(self, nf, abundance):
+        
+        """
+        add abundance to each novel family id
+        """
+
+        if not nf in self.nf_abundance.keys():
+            self.nf_abundance[nf] = abundance
+        else:
+            self.nf_abundance[nf] += abundance
+    
+
+    def calculate_nf_abundance(self, nf_dict:dict):
+       
+        if NovelFam_sample.option_unit == 'TPM':
+
+            for nf in self.nf_abundance.keys():
+                nf_dict = check_key(nf_dict, nf, NovelFam_sample.sample_list) 
+                nf_dict[nf][self.samplename] = (self.nf_abundance[nf]/self.total)*10**6
+                
+            nf_dict = check_unmapped(nf_dict, NovelFam_sample.sample_list, des=False)
+            #tpm_mapped_og = (self.mapped_og/self.total)*10**6 # MAPPED IN TPM
+            #og_dict['UNMAPPED'][self.samplename] = 1000000 * (self.total - self.mapped_og)/self.total # es lo mismo
+            nf_dict['UNMAPPED'][self.samplename] = 1000000 - (self.mapped/self.total)*10**6
+
+        else: 
+
+            for nf in self.nf_abundance.keys():
+
+                nf_dict = check_key(nf_dict, nf, NovelFam_sample.sample_list)    
+                nf_dict[nf][self.samplename] = self.nf_abundance[nf]/self.total
+            
+            nf_dict = check_unmapped(nf_dict, NovelFam_sample.sample_list, des=False)
+            nf_dict['UNMAPPED'][self.samplename] = 1 - self.mapped/self.total
+
+        
+        return nf_dict
+    
+
+    def calc_contig_nf_abundance(self, nf, contig, TPM, nf_dict2):
+   
+        # if the og is not in the global og_dict initialize it in the dictionary
+        if nf not in nf_dict2.keys():
+            nf_dict2[nf] = {}
+            for sample in NovelFam_sample.sample_list:
+                nf_dict2[nf][sample] = 0 # initialize to 0 in all samples
+
+        # check if the nf is initialize in the sample's dictionary    
+        if nf not in self.contig_nf.keys():
+            self.contig_nf[nf] = {}
+            self.contig_nf[nf]['total'] = 0 # initialize total to 0
+
+        # if the contig hasn't been sum
+        if contig not in self.contig_nf[nf].keys():
+            nf_dict2[nf][self.samplename] += float(TPM)  # add to global dict   
+            
+            self.contig_nf[nf][contig] = float(TPM)    # add contig abundance in sample dict
+            self.contig_nf[nf]['total'] += float(TPM)  # add to sample dict
+
+        return nf_dict2
